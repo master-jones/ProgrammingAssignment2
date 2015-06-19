b5c03bfd08315bf134268255f426f1ce00a98b4c
master-jones <master-jones@users.noreply.github.com>
2015-06-19 19:10:31 +0200

    implemented matrix cache and inverse matrix calculation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The following methods provide functionality to calculate 
+# the inverse of a matrix or retrieve the result from the cache
+# if the calculation has already taken place.
 
-## Write a short comment describing this function
-
+# Special function to manage the original matrix as well as 
+# the calculated inverse matrix. Provides necessary getter 
+# and setter methods and acts as a cache.
+# 
+# Args:
+#   x: The original matrix from which to calculate the inverse matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Holds the calculated inverse matrix
+  inverse_matrix <- NULL
+  
+  # Setter to update the original matrix and 
+  # reset the old inverse matrix
+  # 
+  # Args:
+  #   new_matrix: the new matrix
+  set <- function(new_matrix) {
+    x <<- new_matrix
+    inverse_matrix <<- NULL
+  }
+  
+  # Returns the current matrix 
+  get <- function() x
+  
+  # Updates the inverse matrix object
+  # 
+  # Args:
+  #   new_inverse_matrix: The newly calculated inverse matrix
+  setinverse <- function(new_inverse_matrix) inverse_matrix <<- new_inverse_matrix
+  
+  # Returns the cached inverse matrix
+  getinverse <- function() inverse_matrix
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+# If the inverse matrix has not been calculated it will
+# calculate the inverse of the given matrix, otherwise 
+# the inverse matrix will be retrieved from the cache.
+#
+# Args:
+#   x: Special function (list) that holds the matrix and acts as cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse_matrix <- x$getinverse()
+  
+  # check if inverse matrix has already been calculated
+  if(!is.null(inverse_matrix)) {
+    message("getting cached inverse matrix")
+    # return cached result
+    return(inverse_matrix)
+  }
+  
+  # get original matrix and calculate inverse matrix
+  org_matrix <- x$get()
+  inverse_matrix <- solve(org_matrix, ...)
+  x$setinverse(inverse_matrix)
+  # return inverse matrix
+  inverse_matrix
 }
